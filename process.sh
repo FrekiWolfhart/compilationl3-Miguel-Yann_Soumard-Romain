09438e2eb91f9e0ef42fd0317cf6dec4af31d935
romain <romain.soumard@etu.univ-amu.fr>
2020-02-05 19:18:49 +0100

    Amélioration du script

--- process.sh
+++ process.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
 
+
+testAllFiles() {
+    cd src
+    for file in $(ls ../test/input); do
+        echo -n "$file: "
+        java Compiler ../test/input/$file
+    done
+
+    cd ..
+    cd test/input
+    rm *.sc
+    cd ../..
+}
+#find -name *.class -delete
+
 java -jar sablecc.jar src/grammaireL.sablecc
 cd src
 javac Compiler.java
-
-for file in $(ls ../test/input); do
-    echo -n "$file: "
-    java Compiler ../test/input/$file
-done
-
 cd ..
-cd test/input
-rm *.sc
 
-cd ../..
-#find -name *.class -delete
+[ $# -eq 0 ] && exit 0
+
+if [ $1 == "-test" ]; then
+    echo "Test"
+    testAllFiles
+fi
+
+exit 0
