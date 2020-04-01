2f86a6fe98bd0f2c0255a2824816574f94c786b7
FrekiWolfhart <yoshii13@yahoo.fr>
2020-04-01 21:02:41 +0200

    Ajout du fichier Sa2ts.

---
+++ src/Sa2ts.java
@@ -0,0 +1,28 @@
+import sa.*;
+
+public class Sa2ts extends SaDepthFirstVisitor<Void>{
+    
+    @Override
+    Void visit(SaDecVar node){
+    }
+
+    @Override
+    Void visit(SaDecTab node){
+    }
+
+    @Override
+    Void visit(SaDecFonc node){
+    }
+
+    @Override
+    Void visit(SaVarSimple node){
+    }
+
+    @Override
+    Void visit(SaVarIndicee node){
+    }
+
+    @Override
+    Void visit(SaAppel node){
+    }
+}
